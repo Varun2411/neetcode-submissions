6039ced26c84120913c060442a81b9e74ddf5484
Varun2411 <120203192+Varun2411@users.noreply.github.com>
2026-04-19 16:13:19 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()){
+            return false;
+        }
+        vector<int>count(26,0);
+        for(int i = 0;i<s.length();i++){
+            count[s[i]-'a']++;
+            count[t[i]-'a']--;
+        }
+
+
+        for(auto x: count){
+            if(x!=0){
+                return false;
+            }
+        }
+
+    return true;
+    }
+};
