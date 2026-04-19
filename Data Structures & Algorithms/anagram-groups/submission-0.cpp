45033d27cf8106832120cee4395207376be69c82
Varun2411 <120203192+Varun2411@users.noreply.github.com>
2026-04-19 17:34:42 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mp;
+        for(auto x:strs){
+            vector<int>count(26,0);
+            for (char c:x){
+                count[c-'a']++;
+            }
+            string key = to_string(count[0]);
+            for(int i =1 ; i<26 ;i++){
+                key+= ','+to_string(count[i]);
+            }
+            mp[key].push_back(x);
+        }
+        vector<vector<string>> ans;
+        for(auto x: mp){
+            ans.push_back(x.second);
+        }
+        return ans;
+    }
+};
